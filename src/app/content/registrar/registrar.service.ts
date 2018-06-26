641f607f5a2b9449c18a156e2802fddb2c6104d2
douglasacost <douglasacostaaquino@gmail.com>
2018-06-26 14:49:47 -0400

    url updated

--- src/app/content/registrar/registrar.service.ts
+++ src/app/content/registrar/registrar.service.ts
@@ -6,12 +6,12 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class RegistrarService {
-
+  url: String = 'http://localhost:3000';
   constructor(private http: HttpClient) { }
   saveSolicitante(data: any) {
-    return this.http.get('/solicitudes/last').toPromise().then(expediente => {
+    return this.http.get(this.url + '/solicitudes/last').toPromise().then(expediente => {
       if (expediente) {
-        this.http.post('/solicitudes', {...data, expediente});
+        this.http.post(this.url + '/solicitudes', {...data, expediente});
       } else {
         return null;
       }
