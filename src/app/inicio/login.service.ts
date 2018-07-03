a6f3dc1723a428b263ca982fc13c2754fdfe3b82
douglasacost <douglasacostaaquino@gmail.com>
2018-07-03 14:26:20 -0400

    url updated in login service

--- src/app/inicio/login.service.ts
+++ src/app/inicio/login.service.ts
@@ -6,14 +6,14 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
-
+  url: String = 'http://localhost:3000';
   constructor(private http: HttpClient) { }
   saveUser(data: any): Observable<any> {
-    return this.http.post('/user', data)
+    return this.http.post(this.url + '/user', data);
   }
 
   loginUser(data: any): Observable<any> {
-    return this.http.post('/user/login', data)
+    return this.http.post(this.url + '/user/login', data);
   }
 }
 
